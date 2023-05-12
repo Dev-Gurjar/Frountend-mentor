09e20492aabf8b9498caf1c139f9bb4b03503003
Devendra Gurjar <112057881+Dev-Gurjar@users.noreply.github.com>
2023-05-12 20:58:49 +0530

    ERROR

--- javascript.js
+++ javascript.js
@@ -3,51 +3,84 @@ function tri() {
     var inday = document.getElementById("inday").value;
     var inmonth = document.getElementById("inmonth").value;
     var inyear = document.getElementById("inyear").value;
-    
+
     var now = new Date();
-    
-    var currentyear = now.getFullYear();  
-    var currentmonth = now.getMonth();  
+
+    var currentyear = now.getFullYear();
+    var currentmonth = now.getMonth();
     var currentday = now.getDay();
 
-    // console.log(currentday);
-    // console.log(currentmonth);
-    // console.log(currentyear);
+    // console.log(inday);
+    // console.log(inmonth);
+    // console.log(inyear);
+    var flag1 = 1, flag2 = 1, flag3 = 1;
 
-    var year = currentyear-inyear;
-    
-    if(currentmonth>=inmonth)
-        var month = currentmonth-inmonth;
-    else{
-        year--;
-        var month = 12+currentmonth-inmonth;
+    if (inday > 31 || inday < 0 || inday == '') {
+        var x = document.getElementById("error1");
+        x.style.display = "block";
+    }
+    else {
+        var x = document.getElementById("error1");
+        x.style.display = "none";
+        flag1 = 0;
     }
 
-    if(currentday>=inday)
-        var day = currentday-inday;
-    else{
-        var day = 31+currentday-inday;
+    if (inmonth > 12 || inmonth < 0 || inmonth == '') {
+        var x = document.getElementById("error2");
+        x.style.display = "block";
+    }
+    else {
+        var x = document.getElementById("error2");
+        x.style.display = "none";
+        flag2 = 0;
+    }
+
+    if (inyear > 2023 || inyear < 0 || inyear == '') {
+        var x = document.getElementById("error3");
+        x.style.display = "block";
+    }
+    else {
+        var x = document.getElementById("error3");
+        x.style.display = "none";
+        flag3 = 0;
+    }
+
+
+
+    var year = currentyear - inyear;
+
+    if (currentmonth >= inmonth)
+        var month = currentmonth - inmonth;
+    else {
+        year--;
+        var month = 12 + currentmonth - inmonth;
+    }
+
+    if (currentday >= inday)
+        var day = currentday - inday;
+    else {
+        var day = 31 + currentday - inday;
         // month--;
 
-        if(month<0){
+        if (month < 0) {
             // console.log("asdf");
-            month=11;
+            month = 11;
             year--;
         }
     }
-    // month++;
-    document.getElementById("1").innerHTML= day + "&nbsp;";
-    document.getElementById("2").innerHTML= month + "&nbsp;";
-    document.getElementById("3").innerHTML= year + "&nbsp;";
-    
-//     console.log(now);
+    if (flag1 == 0 && flag2 == 0 && flag3 == 0) {
+        document.getElementById("1").innerHTML = day + "&nbsp;";
+        document.getElementById("2").innerHTML = month + "&nbsp;";
+        document.getElementById("3").innerHTML = year + "&nbsp;";
+    }
+    //     console.log(now);
 
-//     console.log(inday);
-//     console.log(inmonth);
-//     console.log(inyear);
+    //     console.log(inday);
+    //     console.log(inmonth);
+    //     console.log(inyear);
 
-//     console.log(day);
-//     console.log(month);
-//     console.log(year);
+    //     console.log(day);
+    //     console.log(month);
+    //     console.log(year);
 }
 
